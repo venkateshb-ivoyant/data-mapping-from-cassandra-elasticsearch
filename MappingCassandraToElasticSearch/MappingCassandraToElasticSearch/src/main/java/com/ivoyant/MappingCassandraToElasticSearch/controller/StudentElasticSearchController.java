0bbdad07e5ffac1add2e96c8290a0618f62cf927
venkateshb-ivoyant <138674679+venkateshb-ivoyant@users.noreply.github.com>
2023-08-19 16:05:11 +0530

    Added the more end points to perform Crud Operations on Migrated data from cassandra on elastic-search

--- MappingCassandraToElasticSearch/MappingCassandraToElasticSearch/src/main/java/com/ivoyant/MappingCassandraToElasticSearch/controller/StudentElasticSearchController.java
+++ MappingCassandraToElasticSearch/MappingCassandraToElasticSearch/src/main/java/com/ivoyant/MappingCassandraToElasticSearch/controller/StudentElasticSearchController.java
@@ -6,12 +6,13 @@ import com.ivoyant.MappingCassandraToElasticSearch.entity.Student;
 import com.ivoyant.MappingCassandraToElasticSearch.repository.StudentElasticSearchRepository;
 import com.ivoyant.MappingCassandraToElasticSearch.service.CassandraStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -58,6 +59,36 @@ public class StudentElasticSearchController {
         // ... other properties
 
         return elasticStudent;
+    }
+    @GetMapping("getAllStudents")
+    public  Iterable<ElasticStudent> getAllStudents(){
+       return studentElasticSearchRepository.findAll();
+    }
+    @GetMapping("getStudent/{uuid}")
+    public Optional<ElasticStudent> getStudentById(@PathVariable UUID uuid){
+       return studentElasticSearchRepository.findById(uuid);
+    }
+
+
+    @PutMapping("updateStudent/{uuid}")
+    public ElasticStudent updateStudent(@PathVariable UUID uuid,@RequestBody ElasticStudent elasticStudent){
+       ElasticStudent elasticStudent1 = studentElasticSearchRepository.findById(uuid).get();
+       elasticStudent1.setName(elasticStudent.getName());
+       elasticStudent1.setAddress(elasticStudent.getAddress());
+       elasticStudent1.setGrade(elasticStudent.getGrade());
+       elasticStudent1.setSubjects(elasticStudent.getSubjects());
+       return studentElasticSearchRepository.save(elasticStudent1);
+    }
+
+    @DeleteMapping("deleteStudent/{uuid}")
+    public String deleteStudent(@PathVariable UUID uuid){
+       ElasticStudent elasticStudent = studentElasticSearchRepository.findById(uuid).get();
+       if (elasticStudent == null){
+           return "Student Not Found";
+       }else {
+           studentElasticSearchRepository.deleteById(uuid);
+           return "Student Successfully Deleted";
+       }
     }
 
 
